93c172176e63a1496cb14e391ff61d98ab808dd4
Nathan Hartman <nathanial.hartman@gmail.com>
2017-02-23 03:12:50 -0800

    added widget controls

--- src/App.js
+++ src/App.js
@@ -2,14 +2,45 @@ import React, { Component } from 'react';
 import Timeline from './timeline/Timeline';
 import './App.css';
 
+function DemoControls(props){
+	return (
+		<div className="demo-controls">
+			<div className="add-widget demo-control"
+					onClick={props.addWidget}>
+				Add Widget
+			</div>
+		</div>
+	);
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Timeline />
-      </div>
-    );
+  constructor(){
+  	super(...arguments);
+    this.state = {
+      widgets: [
+				{name: 'Widget 1'},
+				{name: 'Widget 2'},
+				{name: 'Widget 3'}
+			]
+    };
   }
+
+	render() {
+		return (
+			<div className="App">
+				<Timeline widgets={this.state.widgets} />
+				<DemoControls addWidget={this.addWidget}  />
+			</div>
+		);
+	}
+
+	addWidget = () => {
+  	this.setState({
+  		widgets: this.state.widgets.concat({
+  			name: 'Widget ' + (this.state.widgets.length + 1)
+			})
+		});
+	}
 }
 
 export default App;
--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import './Timeline.css';
 
-class Timeline extends Component {
+function TimelineWidget(props){
+	return (
+			<div className="timeline-widget">
+				<span className="timeline-widget-name">
+					{props.widget.name}
+				</span>
+			</div>
+	);
+}
+
+export default class Timeline extends Component {
+	static propTypes = {
+		widgets: React.PropTypes.array.isRequired
+	};
+
+	static defaultProps = {
+		widgets: []
+	};
+
 	render(){
 		return (
-				<div>
-					<h1>Foo</h1>
+			<div className="timeline">
+				<div className="timeline-widgets">
+					{this.props.widgets.map(widget => {
+						return <TimelineWidget key={widget.name} widget={widget}/>
+					})}
 				</div>
+			</div>
 		);
 	}
 }
