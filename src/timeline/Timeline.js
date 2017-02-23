cfc54f13e226c696aa0e0c97d465be5ada107472
Nathan Hartman <nathanial.hartman@gmail.com>
2017-02-23 02:54:09 -0800

    setting up timeline widget

---
+++ src/timeline/Timeline.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Timeline.css';
+
+class Timeline extends Component {
+	render(){
+		return (
+				<div>
+					<h1>Foo</h1>
+				</div>
+		);
+	}
+}
